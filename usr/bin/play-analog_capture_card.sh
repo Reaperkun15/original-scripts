ac7b1b71833bbe0195395910c5d014d6baea72c7
りーぱー <119842878+Reaperkun15@users.noreply.github.com>
2026-07-02 20:23:50 +0900

    Add notice for existing device setup
    Added notice for reconfiguring device settings.

--- usr/bin/play-analog_capture_card.sh
+++ usr/bin/play-analog_capture_card.sh
@@ -16,6 +16,7 @@ MPV_ADD_OPTION='--video-aspect-override=4:3'
 echo "Setting up device"
 if [ -e /tmp/v4l2-setup ]; then
     echo "Warning: It appears to have already been set up. Skipping"
+    echo 'Notice: To reconfigure the device settings, delete "/tmp/v4l2-setup"'
 else
     v4l2-ctl -d ${DEVICE} --set-standard=ntsc --set-fmt-video=width=720,height=480,pixelformat=UYVY --set-parm=30000/1001
     v4l2-ctl -d ${DEVICE}  -i ${TARGET_INPUT}
